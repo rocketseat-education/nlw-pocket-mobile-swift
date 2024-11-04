20759d1c85c0010d9650a16f5eefdcbe1795ace0
Mateus Coelho <teus.hcp@gmail.com>
2024-11-04 14:29:39 -0300

    Initial Commit

---
+++ NearbyApp NLW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearbyApp NLW
+//
+//  Created by Mateus Henrique Coelho de Paulo on 04/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
